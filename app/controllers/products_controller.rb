1a756e26894329ccab78a5bec7446474616b97d2
Leigh Turland <wlturland@gmail.com>
2015-08-26 08:40:06 -0500

    Day 2 morning commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -69,6 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :category_id, :brand_id, :description, :price, :quantity)
+      params.require(:product).permit(:name, :category_id, :brand_id, :description, :price, :quantity, :image)
     end
 end
